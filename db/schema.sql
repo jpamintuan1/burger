cbeb6fcd1e867a5e81eac179cf85b4e27ddd0a53
Joseph Pamintuan <Joseph Pamintuan>
2017-07-29 10:09:59 -0700

    first

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bilal');
+INSERT INTO burgers (burger_name) VALUES ('Brianne');
+INSERT INTO burgers (burger_name) VALUES ('Vincent');
+
